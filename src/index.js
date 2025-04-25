6ee3095ffc61686920d035c8e634114125b8651a
westford14 <westford14@gmail.com>
2025-04-25 17:11:51 +0200

    fixing the package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<App />);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
