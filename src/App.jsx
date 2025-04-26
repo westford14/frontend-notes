27d9d6a0f413f2ec37423071c755e15c17e24aa0
westford14 <westford14@gmail.com>
2025-04-26 15:54:38 +0200

    Adding very simple auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,19 @@
-import React from "react";
-import { nanoid } from "nanoid";
-import { useState, useEffect } from "react";
-import NotesList from "./components/NotesList";
-import Search from "./components/Search";
-import Header from "./components/Header";
-const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: nanoid(),
-      text: "This is my first note!",
-      date: "02/14/2022",
-    },
-    {
-      id: nanoid(),
-      text: "This is my second note!",
-      date: "02/10/2022",
-    },
-    {
-      id: nanoid(),
-      text: "This is my third note!",
-      date: "02/12/2022",
-    },
-  ]);
+import React, { useState } from "react";
+import LoginPage from "./components/LoginPage";
+import MainApp from "./components/MainApp";
 
-  const [searchText, setSearchText] = useState("");
-
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
-
-    if (savedNotes) {
-      setNotes(savedNotes);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
-  }, [notes]);
-
-  const addNote = (text) => {
-    const date = new Date();
-    const newNote = {
-      id: nanoid,
-      text: text,
-      date: date.toLocaleDateString(),
-    };
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
-  };
-
-  const deleteNote = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-  };
-
-  useEffect(() => {
-    const user = AuthService.getCurrentUser();
-
-    if (user) {
-      setCurrentUser(user);
-      setShowModeratorBoard(user.roles.includes("ROLE_MODERATOR"));
-      setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
-    }
-
-    EventBus.on("logout", () => {
-      logOut();
-    });
-
-    return () => {
-      EventBus.remove("logout");
-    };
-  }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-    setShowModeratorBoard(false);
-    setShowAdminBoard(false);
-    setCurrentUser(undefined);
-  };
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <div className={`${darkMode && "dark-mode"}`}>
-      <div className="container">
-        <Header handleToggleDarkMode={setDarkMode} />
-        <Search handleSearchNote={setSearchText} />
-        <NotesList
-          notes={notes.filter((note) =>
-            note.text.toLowerCase().includes(searchText),
-          )}
-          handleAddNote={addNote}
-          handleDeleteNote={deleteNote}
-        />
-      </div>
+    <div>
+      {!isLoggedIn ? (
+        <LoginPage onLoginSuccess={() => setIsLoggedIn(true)} />
+      ) : (
+        <MainApp />
+      )}
     </div>
   );
-};
+}
 
 export default App;
